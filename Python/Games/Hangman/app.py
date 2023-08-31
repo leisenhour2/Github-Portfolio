142ecf54ac89c79d33584e623cc982584aac9ff7
leisenhour2 <124882440+leisenhour2@users.noreply.github.com>
2023-08-31 06:38:26 -0400

    Small Edit
    Instead Of Using {len(wordlist)} in line 13 of "app.py" code. I changed it to use function "wordcount()" instead which pretty much does the same thing, just with a function call.

--- Python/Games/Hangman/app.py
+++ Python/Games/Hangman/app.py
@@ -10,7 +10,7 @@ aiscore = 0 #AI's Total Score
 
 #Function That Prints Out A Scoreboard With The Current Scores Updated
 def scoreboard():
-    return f"""WORDS LEFT IN WORDLIST: {len(wordlist)}
+    return f"""WORDS LEFT IN WORDLIST: {wordcount()}
             =======================
                 SCORES:            
                 Player: {pscore}   
